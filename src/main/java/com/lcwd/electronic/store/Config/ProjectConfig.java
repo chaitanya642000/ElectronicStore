5b91e57e92609abbba0a37275565a9e6a9b29687
chaitanya642000 <chaitanya.gujarathi06@gmail.com>
2023-06-09 00:17:52 +0530

    Added mapper

---
+++ src/main/java/com/lcwd/electronic/store/Config/ProjectConfig.java
@@ -0,0 +1,17 @@
+package com.lcwd.electronic.store.Config;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public ModelMapper mapper()
+    {
+        return new ModelMapper();
+    }
+
+}
